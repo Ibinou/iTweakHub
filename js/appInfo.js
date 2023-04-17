65f6676693a5c837a61e0433aa8395898b99638c
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-04-17 17:25:54 +0200

    Fixed some app info

---
+++ js/appInfo.js
@@ -0,0 +1,9 @@
+const queryString = window.location.search;
+console.log(queryString);
+
+urlParams = new URLSearchParams(queryString);
+console.log(urlParams.get('name'))
+document.getElementById("appName").innerHTML = urlParams.get('name')
+document.getElementById("appDeveloper").innerHTML = urlParams.get('developer')
+document.getElementById("appDescription").innerHTML = urlParams.get('description')
+document.title = urlParams.get('name')
--- js/index.js
+++ js/index.js
@@ -1,30 +1,18 @@
+
+
 function addRepo(repo) {
     alert("Work in progress, installed repo: ", repo.value)
 }
 
-function openApp(name, developer) {
-    window.open("appinfos.html", "_parent")
-    document.getElementById("appName").innerHTML = name
-    document.getElementById("appDeveloper").innerHTML = developer
-    document.title = name
+function openApp(name, developer, description) {
+    window.open("appinfos.html?name=" + name + "&developer=" + developer + "&description=" + description, "_parent")
 }
 
-_iOSDevice = !!navigator.platform.match(/iPhone|iPod|iPad/);
-if(_iOSDevice) {
- _iOSVersion = (navigator.userAgent.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/)||[''])[0].replace(/_/g,'.');
-} else {
-    _iOSVersion = ""
+
+function loadAppInfo(name, developer) {
 }
 
-var signed = true
 
-if(signed) {
-    document.getElementById("signed").style.color = "#00c227"
-} else {
-    document.getElementById("signed").style.color = "red"
-}
-
-document.getElementById('iosVer').innerHTML = _iOSVersion
 
 function reloadRepos() {
     alert("Work in progress")
