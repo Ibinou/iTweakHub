a71e9be4b63b53c14917f7d40ae3efdf0b540870
Kevin Alavik <kevin@alavik.se>
2023-04-17 06:39:54 +0000

    Added placeholder apps, to prepare for repo handler

--- index.js
+++
@@ -1,3 +0,0 @@
-function addRepo(repo) {
-    
-}
---
+++ js/index.js
@@ -0,0 +1,7 @@
+function addRepo(repo) {
+    alert("Work in progress, installed repo: ", repo.value)
+}
+
+function openApp(name, developer) {
+    window.open("appinfos.html")
+}
