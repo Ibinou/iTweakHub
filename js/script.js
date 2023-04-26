df64a23a170118ee1cb723c9b5c3614ed0e5b2c5
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-04-26 18:42:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username === "developer" && password === "access") {
+    if (username === "dev" && password === "enter") {
         document.getElementById("login").style.display = "none";
-        document.getElementById("body").style.display = "block";
+        document.getElementById("content").style.display = "block";
     } else {
         alert("Invalid login credentials.");
     }
-}    
+}
