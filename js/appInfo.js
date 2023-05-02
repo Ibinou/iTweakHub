b83bf6fbe4ab14c6692cd365fb11ce592e381fca
Kevin Alavik <kevin@alavik.se>
2023-05-02 10:32:04 +0200

    Update appInfo.js

--- js/appInfo.js
+++ js/appInfo.js
@@ -1,6 +1,4 @@
 const queryString = window.location.search;
-console.log(queryString);
-
 urlParams = new URLSearchParams(queryString);
 console.log(urlParams.get('name'))
 document.getElementById("appName").innerHTML = urlParams.get('name')
@@ -9,3 +7,4 @@ document.getElementById("appDescription").innerHTML = urlParams.get('description
 document.getElementById("appBanner").src = urlParams.get('banner')
 document.getElementById("appIcon").src = urlParams.get('icon')
 document.title = "iTweakHub | " + urlParams.get('name')
+window.alert(urlParams)
