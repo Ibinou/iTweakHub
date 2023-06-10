d49b7bd743a7c16f072ef7e62db6245aa6a23482
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-06-10 13:32:06 +0200

    Update account.js

--- js/account.js
+++ js/account.js
@@ -8,6 +8,44 @@ const usernameDisplay = document.getElementById('username-display');
 const createAccountBtn = document.getElementById('create-account-btn');
 const logoutBtn = document.getElementById('logout-btn');
 
+// Fonction pour créer un cookie
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+// Fonction pour récupérer la valeur d'un cookie
+function getCookie(name) {
+  const cookieArr = document.cookie.split(';');
+  for (let i = 0; i < cookieArr.length; i++) {
+    const cookiePair = cookieArr[i].split('=');
+    if (cookiePair[0].trim() === name) {
+      return cookiePair[1];
+    }
+  }
+  return null;
+}
+
+// Fonction pour supprimer un cookie
+function deleteCookie(name) {
+  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`;
+}
+
+// Vérifier si un cookie d'utilisateur existe
+const usernameCookie = getCookie('username');
+const profilePictureCookie = getCookie('profilePicture');
+
+if (usernameCookie && profilePictureCookie) {
+  // Afficher les informations du cookie
+  profilePicturePreview.src = profilePictureCookie;
+  usernameDisplay.textContent = usernameCookie;
+
+  // Masquer la création de compte et afficher la page d'accueil
+  loginContainer.style.display = 'none';
+  mainContainer.style.display = 'block';
+}
+
 // Événement lors de la création du compte
 createAccountBtn.addEventListener('click', () => {
   const username = usernameInput.value;
@@ -18,6 +56,10 @@ createAccountBtn.addEventListener('click', () => {
     alert('Veuillez saisir un pseudo.');
     return;
   }
+
+  // Enregistrer les informations dans les cookies
+  setCookie('username', username, 7); // Le cookie expire après 7 jours
+  setCookie('profilePicture', URL.createObjectURL(profilePicture), 7);
 
   // Afficher les informations sur la page d'accueil
   profilePicturePreview.src = URL.createObjectURL(profilePicture);
@@ -30,6 +72,10 @@ createAccountBtn.addEventListener('click', () => {
 
 // Événement lors de la déconnexion
 logoutBtn.addEventListener('click', () => {
+  // Supprimer les cookies
+  deleteCookie('username');
+  deleteCookie('profilePicture');
+
   // Réinitialiser les champs de saisie et les informations affichées
   usernameInput.value = '';
   profilePictureInput.value = '';
