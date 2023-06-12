ad39ea886becd8fd0acfc7eaded84d999eb14ca4
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-06-12 16:44:03 +0200

    Update transition.js

--- js/transition.js
+++ js/transition.js
@@ -1,5 +1,21 @@
-    window.addEventListener('DOMContentLoaded', function() {
+// Tabs transition
+window.addEventListener('DOMContentLoaded', function() {
       var container = document.getElementById('transitionbody');
       container.classList.remove('hidden');
       container.classList.add('fade-in');
     });
+
+
+// Page Swithcing by Kevin Alavik
+let displayType = 'flex';
+
+function switchPage(page) {
+  const pages = document.querySelectorAll('.page-container');
+    for (let i = 0; i < pages.length; i++) {
+      if (pages[i].getAttribute('page') === page) {
+        pages[i].style.display = displayType;
+      } else {
+        pages[i].style.display = 'none';
+      }
+   }
+}
