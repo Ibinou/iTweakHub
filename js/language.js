afe891610f69e707aaa9ac40e2f0aed3b6236e14
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-07-07 13:56:00 +0200

    Create language.js

---
+++ js/language.js
@@ -0,0 +1,33 @@
+// Fonction pour charger les traductions
+function loadTranslations(language) {
+  fetch(`${language}.json`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('home').innerText = data.home;
+    });
+}
+
+// Fonction pour changer la langue
+function changeLanguage(language) {
+  loadTranslations(language);
+  localStorage.setItem('language', language);
+}
+
+// Vérifie si la langue est déjà enregistrée dans le local storage
+var savedLanguage = localStorage.getItem('language');
+if (savedLanguage) {
+  changeLanguage(savedLanguage);
+} else {
+  // Chargement initial des traductions (langue par défaut)
+  changeLanguage('en');
+}
+
+// Fonction pour changer la langue en anglais
+document.getElementById('btn-en').addEventListener('click', function() {
+  changeLanguage('en');
+});
+
+// Fonction pour changer la langue en français
+document.getElementById('btn-fr').addEventListener('click', function() {
+  changeLanguage('fr');
+});
