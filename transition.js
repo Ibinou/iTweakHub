c01cdbb18bbe344d3aaf507719ba96df5a655e6a
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-07-07 14:00:05 +0200

    Delete transition.js

--- transition.js
+++
@@ -1,5 +0,0 @@
-   window.addEventListener('DOMContentLoaded', function() {
-      var container = document.getElementById('transitionbody');
-      container.classList.remove('hidden');
-      container.classList.add('fade-in');
-    });
