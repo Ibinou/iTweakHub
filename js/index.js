c312e66bb8534fa067b544a4e076f833b35a6017
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-07-24 13:56:59 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -112,3 +112,28 @@ function loadAll() {
   
 loadAll();
 
+
+// THEME SCRIPT
+        // function to set a given theme/color-scheme
+        function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.documentElement.className = themeName;
+        }
+
+        // function to toggle between light and dark theme
+        function toggleTheme() {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-light');
+            } else {
+                setTheme('theme-dark');
+            }
+        }
+
+        // Immediately invoked function to set the theme on initial load
+        (function () {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-dark');
+            } else {
+                setTheme('theme-light');
+            }
+        })();
