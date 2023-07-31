66cd78937ca6ca0678d5ba53d0d98c7a436fb76e
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-07-31 15:02:35 +0200

    Update account.js

--- js/account.js
+++ js/account.js
@@ -1,73 +1,72 @@
-        // Variables pour les éléments HTML
-        const loginContainer = document.getElementById('login-container');
-        const mainContainer = document.getElementById('main-container');
-        const usernameInput = document.getElementById('username');
-        const profilePictureInput = document.getElementById('profile-picture');
-        const profilePicturePreview = document.getElementById('profile-picture-preview');
-        const usernameDisplay = document.getElementById('username-display');
-        const createAccountBtn = document.getElementById('create-account-btn');
-        const logoutBtn = document.getElementById('logout-btn');
+// Variables pour les éléments HTML
+const loginContainer = document.getElementById('login-container');
+const mainContainer = document.getElementById('main-container');
+const usernameInput = document.getElementById('username');
+const profilePictureInput = document.getElementById('profile-picture');
+const profilePicturePreview = document.getElementById('profile-picture-preview');
+const usernameDisplay = document.getElementById('username-display');
+const createAccountBtn = document.getElementById('create-account-btn');
+const logoutBtn = document.getElementById('logout-btn');
 
-        // Vérifier si l'utilisateur est déjà connecté
-        if (localStorage.getItem('username') && localStorage.getItem('profilePicture')) {
-            const username = localStorage.getItem('username');
-            const profilePicture = localStorage.getItem('profilePicture');
+// Vérifier si l'utilisateur est déjà connecté
+if (localStorage.getItem('username') && localStorage.getItem('profilePicture')) {
+  const username = localStorage.getItem('username');
+  const profilePicture = localStorage.getItem('profilePicture');
 
-            // Afficher les informations sur la page d'accueil
-            profilePicturePreview.src = profilePicture;
-            usernameDisplay.textContent = username;
+  // Afficher les informations sur la page d'accueil
+  profilePicturePreview.src = profilePicture;
+  usernameDisplay.textContent = username;
 
-            // Masquer la création de compte et afficher la page d'accueil
-            loginContainer.style.display = 'none';
-            mainContainer.style.display = 'block';
-        } else {
-            // L'utilisateur n'est pas connecté, donc afficher la division de connexion
-            loginContainer.style.display = 'flex';
-            mainContainer.style.display = 'none';
-        }
+  // Masquer la création de compte et afficher la page d'accueil
+  loginContainer.style.display = 'none';
+  mainContainer.style.display = 'block';
+} else {
+  // Si l'utilisateur n'est pas connecté, afficher la page welcome.html
+  window.location.href = 'welcome.html';
+}
 
-        // Événement lors de la création du compte
-        createAccountBtn.addEventListener('click', () => {
-            const username = usernameInput.value;
-            const profilePicture = profilePictureInput.files[0];
+// Événement lors de la création du compte
+createAccountBtn.addEventListener('click', () => {
+  const username = usernameInput.value;
+  const profilePicture = profilePictureInput.files[0];
 
-            // Vérifier si un pseudo a été saisi
-            if (username.trim() === '') {
-                alert('Veuillez saisir un pseudo.');
-                return;
-            }
+  // Vérifier si un pseudo a été saisi
+  if (username.trim() === '') {
+    alert('Veuillez saisir un pseudo.');
+    return;
+  }
 
-            // Enregistrer les informations dans le localStorage
-            localStorage.setItem('username', username);
+  // Enregistrer les informations dans le localStorage
+  localStorage.setItem('username', username);
 
-            const reader = new FileReader();
-            reader.onload = function () {
-                localStorage.setItem('profilePicture', reader.result);
+  const reader = new FileReader();
+  reader.onload = function () {
+    localStorage.setItem('profilePicture', reader.result);
 
-                // Afficher les informations sur la page d'accueil
-                profilePicturePreview.src = reader.result;
-                usernameDisplay.textContent = username;
+    // Afficher les informations sur la page d'accueil
+    profilePicturePreview.src = reader.result;
+    usernameDisplay.textContent = username;
 
-                // Afficher la page d'accueil et masquer la création de compte
-                loginContainer.style.display = 'none';
-                mainContainer.style.display = 'block';
-            };
-            reader.readAsDataURL(profilePicture);
-        });
+    // Afficher la page d'accueil et masquer la création de compte
+    loginContainer.style.display = 'none';
+    mainContainer.style.display = 'block';
+  };
+  reader.readAsDataURL(profilePicture);
+});
 
-        // Événement lors de la déconnexion
-        logoutBtn.addEventListener('click', () => {
-            // Supprimer les informations du localStorage
-            localStorage.removeItem('username');
-            localStorage.removeItem('profilePicture');
+// Événement lors de la déconnexion
+logoutBtn.addEventListener('click', () => {
+  // Supprimer les informations du localStorage
+  localStorage.removeItem('username');
+  localStorage.removeItem('profilePicture');
 
-            // Réinitialiser les champs de saisie et les informations affichées
-            usernameInput.value = '';
-            profilePictureInput.value = '';
-            profilePicturePreview.src = '';
-            usernameDisplay.textContent = '';
+  // Réinitialiser les champs de saisie et les informations affichées
+  usernameInput.value = '';
+  profilePictureInput.value = '';
+  profilePicturePreview.src = '';
+  usernameDisplay.textContent = '';
 
-            // Masquer la page d'accueil et afficher la création de compte
-            loginContainer.style.display = 'flex';
-            mainContainer.style.display = 'none';
-        });
+  // Masquer la page d'accueil et afficher la création de compte
+  loginContainer.style.display = 'flex';
+  mainContainer.style.display = 'none';
+});
