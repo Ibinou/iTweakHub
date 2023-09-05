cb384f946a48c7a81a192068a17f19c75985bad9
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-09-05 14:11:54 +0200

    Update language.js

---
+++ js/language.js
@@ -0,0 +1,6 @@
+var Lang = navigator.language ;
+if (Lang == "fr-FR") {
+document.getElementById("welcome").innerHTML = "Bienvenue";
+document.getElementById("toitweakhub").innerHTML = "Sur iTweakHub";
+document.getElementById("done").innerHTML = "Continuer";
+} 
