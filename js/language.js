9e84fc017b945ea3c2c01d0bfe1be7501f178dde
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-09-05 14:37:25 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -6,4 +6,9 @@ document.getElementById("toitweakhub").innerHTML = "Sur iTweakHub";
 document.getElementById("done").innerHTML = "Continuer";
 document.getElementById("termsandprivacy").innerHTML = "Conditions d'utilisation et politique de confidentialité";
 //index.html
+document.getElementById("wb").innerHTML = "Bienvenue,";
+document.getElementById("home").innerHTML = "Accueil";
+document.getElementById("featured").innerHTML = "En vedette";
+document.getElementById("topapps").innerHTML = "Les plus utilisées";
+document.getElementById("newstuto").innerHTML = "Articles et tutoriels,";
 } 
