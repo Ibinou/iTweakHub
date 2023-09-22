1825d428213dae172d7f64500583886baa680875
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-09-22 19:15:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,54 +1,6 @@
-document.addEventListener('keydown', function(event) {
-
-  if (event.ctrlKey && event.altKey && event.key === 'o') {
-
-    window.open('devs.html', '_blank');
-  }
-});
 
 
-// Repo js
-function addRepo(repo) {
-  alert("Work in progress, installed repo: ", repo.value);
-}
 
-function openApp(name, developer, description, banner, icon) {
-  window.open(
-    "appinfos.html?name=" +
-      name +
-      "&developer=" +
-      developer +
-      "&description=" +
-      description +
-      "&banner=" +
-      banner +
-      "&icon=" +
-      icon,
-    "_parent"
-  );
-}
-
-function loadAppInfo(name, developer) {}
-
-function reloadRepos() {
-  alert("Work in progress");
-}
-
-function displayApp() {
-  alert("App info system is work in progress!");
-}
-
-function removeStylesheets() {
-  var stylesheets = document.getElementsByTagName("link");
-  for (var i = 0; i < stylesheets.length; i++) {
-    if (
-      stylesheets[i].getAttribute("rel") == "stylesheet" &&
-      !stylesheets[i].hasAttribute("keep")
-    ) {
-      stylesheets[i].parentNode.removeChild(stylesheets[i]);
-    }
-  }
-}
 
 function deleteAllCookies() {
   var cookies = document.cookie.split(";");
@@ -61,79 +13,6 @@ function deleteAllCookies() {
   }
 }
 
-function resetTheme() {
-    var cookies = document.cookie.split("; ");
-    var visitedCookieValue = null;
-    for (var i = 0; i < cookies.length; i++) {
-      if (cookies[i].indexOf("visited=") === 0) {
-        visitedCookieValue = cookies[i].substring(8);
-      } else {
-        document.cookie = cookies[i] + "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      }
-    }
-    if (visitedCookieValue !== null) {
-      document.cookie = "visited=" + visitedCookieValue + "; path=/;";
-      location.reload();
-    } else {
-      alert("No need to reset, you haven't selected any other theme than the default one. If you don't like the default theme, you can change it in the settings.");
-    }
-  }
-  
-  
-function loadTheme(file) {
-  document.cookie = file;
-  loadAll();
-}
-
-function loadAll() {
-    var cookies = document.cookie.split("; ");
-    var themeCookie = null;
-    for (var i = 0; i < cookies.length; i++) {
-      if (cookies[i].indexOf("visited=") !== 0) {
-        themeCookie = cookies[i];
-        break;
-      }
-    }
-    if (themeCookie !== null) {
-      removeStylesheets();
-      const link = document.createElement("link");
-      link.href = themeCookie + ".css";
-      link.rel = "stylesheet";
-      console.log("Selected theme found, loading theme...");
-      document.getElementsByTagName("head")[0].appendChild(link);
-      console.log(document.cookie);
-      console.log("Successfully loaded the theme and other things.");
-    } else {
-      console.log("No selected theme was found, going back to regular theme.")
-      console.log(document.cookie);
-      console.log("Successfully loaded the theme and other things.");
-    }
-  }
-  
-loadAll();
-
-//oled theme
-document.addEventListener("DOMContentLoaded", function() {
-  const checkbox = document.getElementById("checkbox");
-
-  // Vérifiez si le thème est enregistré dans les cookies et mettez à jour si nécessaire
-  const theme = getCookie("theme");
-  if (theme === "dark") {
-    applyDarkTheme();
-    checkbox.checked = true;
-  }
-
-  // Ajoutez un écouteur pour suivre les changements de la case à cocher
-  checkbox.addEventListener("change", function() {
-    if (checkbox.checked) {
-      applyDarkTheme();
-      setCookie("theme", "dark", 365); // Enregistrez le thème dans les cookies pour 1 an
-    } else {
-      applyLightTheme();
-      setCookie("theme", "light", 365); // Enregistrez le thème clair dans les cookies pour 1 an
-    }
-  });
-});
 
 // Fonction pour obtenir la valeur d'un cookie
 function getCookie(name) {
@@ -149,14 +28,14 @@ function setCookie(name, value, days) {
   document.cookie = name + "=" + value + ";expires=" + expires.toUTCString();
 }
 
-// Appliquer le thème sombre
-function applyDarkTheme() {
-  document.body.style.backgroundColor = "#000";
-}
-
-// Appliquer le thème clair
-function applyLightTheme() {
-  document.body.style.backgroundColor = "#121212";
-}
+    // Transition
+    window.addEventListener('load', function() {
+        // Sélectionnez le corps de la page
+        var body = document.querySelector('body');
+        
+        // Définissez l'opacité sur 0 avec une transition
+        body.style.opacity = 0;
+        body.style.transition = 'opacity 0.5s ease-in-out';
+    });
 
 
