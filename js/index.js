370bfae747bb59f370a90a2f3d5fe3d9ac3c7e31
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-12-14 19:06:10 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,4 +37,46 @@ function setCookie(name, value, days) {
         body.style.opacity = 1;
         body.style.transition = 'opacity 0.5s ease-in-out';
     });
+  
+  
+  
+  
+
+// Fonction pour ajouter le padding top en prenant en compte la "safe area"
+function addPaddingBasedOnMediaQueries() {
+    var toptitleElement = document.querySelector('.toptitle');
+
+    // iPhone 15 and 15 Pro
+    var iPhone15Query = window.matchMedia("(width: 393px) and (height: 852px) and (-webkit-device-pixel-ratio: 3)");
+    // iPhone 15 Plus and 15 Pro Max
+    var iPhone15PlusQuery = window.matchMedia("(width: 430px) and (height: 932px) and (-webkit-device-pixel-ratio: 3)");
+    // iPhone 14
+    var iPhone14Query = window.matchMedia("(width: 390px) and (height: 844px) and (-webkit-device-pixel-ratio: 3)");
+    // iPhone 14 Plus
+    var iPhone14PlusQuery = window.matchMedia("(width: 428px) and (height: 926px) and (-webkit-device-pixel-ratio: 3)");
+    // iPhone 13 Mini
+    var iPhone13MiniQuery = window.matchMedia("(width: 375px) and (height: 812px) and (-webkit-device-pixel-ratio: 3)");
+    // iPhone 11
+    var iPhone11Query = window.matchMedia("(width: 414px) and (height: 896px) and (-webkit-device-pixel-ratio: 2)");
+    // iPhone 11 Pro Max
+    var iPhone11ProMaxQuery = window.matchMedia("(width: 414px) and (height: 896px) and (-webkit-device-pixel-ratio: 3)");
+
+    // Check each media query and add padding accordingly
+    if (iPhone15Query.matches || iPhone15PlusQuery.matches || iPhone14Query.matches || iPhone14PlusQuery.matches || iPhone13MiniQuery.matches) {
+        // Add padding for iPhone 15, 15 Plus, 14, 14 Plus, 13 Mini
+        var safeAreaHeight = window.innerHeight - document.documentElement.clientHeight;
+        toptitleElement.style.paddingTop = '50px';
+    } else if (iPhone11Query.matches || iPhone11ProMaxQuery.matches) {
+        // Add padding for iPhone 11, 11 Pro Max
+        var safeAreaHeight = window.innerHeight - document.documentElement.clientHeight;
+        toptitleElement.style.paddingTop = '50px';
+    } else {
+        // Reset padding for other devices
+        toptitleElement.style.paddingTop = '0';
+    }
+}
+
+// Call the function initially and on window resize
+window.addEventListener('load', addPaddingBasedOnMediaQueries);
+window.addEventListener('resize', addPaddingBasedOnMediaQueries);
     
