0232c89663c2344a8b0a9ecefe365299e3e47709
iBaDev <93536549+Ibinou@users.noreply.github.com>
2024-04-27 16:49:56 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,90 +1,129 @@
-// Fonction pour charger et afficher les applications de manière paginée
-async function chargerApplications(pageSize, pageNumber) {
-  try {
-    const response = await fetch('https://ibinou.github.io/iTweakHub/apps.json');
-    const data = await response.json();
+ // Fonction pour afficher les données
+function afficherDonnees() {
+  var appListDiv = document.getElementById("appList");
 
-    // Pagination des données
-    const start = (pageNumber - 1) * pageSize;
-    const end = start + pageSize;
-    const paginatedApps = data.apps.slice(start, end);
+  // Fetcher les données de apps.json
+  fetch('https://ibinou.github.io/iTweakHub/apps.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var appsData = data.apps; // Assurez-vous que le format JSON correspond
 
-    // Afficher les applications paginées
-    afficherApplications(paginatedApps);
+      // Triez les applications par nom (ordre alphabétique)
+      appsData.sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
 
-    // Stockage local des données pour une utilisation future
-    localStorage.setItem('appsData', JSON.stringify(data.apps));
+      afficherApplications(appsData);
 
-  } catch (error) {
-    console.log('Error fetching or displaying apps:', error);
+      // Récupérer les URLs des JSON depuis le localStorage
+      var repoURLs = JSON.parse(localStorage.getItem('repoURLs')) || [];
+
+      // Fetcher les données des URLs stockées dans repoURLs
+      var fetchPromises = repoURLs.map(function(url) {
+        return fetch(url)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            var appsDataFromURL = data.apps; 
+
+            // Triez les applications par nom également
+            appsDataFromURL.sort(function(a, b) {
+              return a.name.localeCompare(b.name);
+            });
+
+            afficherApplications(appsDataFromURL, url);
+          })
+          .catch(function(error) {
+            console.log('Error fetching data from URL', error);
+          });
+      });
+
+      // Attendre que toutes les requêtes de fetch se terminent
+      return Promise.all(fetchPromises);
+    })
+    .catch(function(error) {
+      console.log('Error fetching apps.json', error);
+    });
+
+  function afficherApplications(appsData, source) {
+    appsData.forEach(function(appData) {
+      var dockDiv = document.createElement("div");
+      dockDiv.className = "dock";
+
+      var appCellLeftDiv = document.createElement("div");
+      appCellLeftDiv.className = "app_cell_left";
+
+      var appIconImg = document.createElement("img");
+      appIconImg.className = "appicon";
+
+      // Vérifier si appData.iconURL existe
+      if (appData.iconURL) {
+        // Vérifier si le lien d'image est valide
+        appIconImg.onload = function() {
+          // L'image a été chargée avec succès
+        };
+        appIconImg.onerror = function() {
+          // Le lien d'image n'est pas valide, utiliser un placeholder
+          appIconImg.src = "https://github.com/Ibinou/iTweakHub/blob/main/img/blank.JPG?raw=true";
+        };
+        appIconImg.src = appData.iconURL;
+      } else {
+        // Utiliser un placeholder si aucun lien d'image n'est fourni
+        appIconImg.src = "https://github.com/Ibinou/iTweakHub/blob/main/img/blank.JPG?raw=true";
+      }
+
+      appCellLeftDiv.appendChild(appIconImg);
+
+      var appCellMetaDiv = document.createElement("div");
+      appCellMetaDiv.className = "app_cell_meta";
+
+      var appNameDiv = document.createElement("div");
+      appNameDiv.className = "appname";
+      appNameDiv.textContent = appData.name;
+      appCellMetaDiv.appendChild(appNameDiv);
+
+      var appDevDiv = document.createElement("div");
+      appDevDiv.className = "appsection";
+      appDevDiv.textContent = appData.developerName;
+      appCellMetaDiv.appendChild(appDevDiv);
+
+      appCellLeftDiv.appendChild(appCellMetaDiv);
+      dockDiv.appendChild(appCellLeftDiv);
+
+      var appGetDiv = document.createElement("div");
+      appGetDiv.className = "appget";
+
+      var appGetBtn = document.createElement("a");
+
+      // Utiliser l'opérateur ternaire pour définir la valeur de source
+      var sourceValue = source !== undefined ? encodeURIComponent(source) : encodeURIComponent('https://ibinou.github.io/iTweakHub/apps.json');
+      appGetBtn.href = 'appinfos.html?name=' + encodeURIComponent(appData.name) + '&source=' + sourceValue;
+
+      var getBtn = document.createElement("button");
+      getBtn.className = "getbtn";
+      getBtn.textContent = "GET";
+
+      appGetBtn.appendChild(getBtn);
+      appGetDiv.appendChild(appGetBtn);
+      dockDiv.appendChild(appGetDiv);
+
+      appListDiv.appendChild(dockDiv);
+    });
   }
 }
 
-// Vérification périodique des mises à jour du fichier JSON distant
-setInterval(async () => {
-  try {
-    const response = await fetch('https://ibinou.github.io/iTweakHub/apps.json');
-    const latestData = await response.json();
-    const storedData = JSON.parse(localStorage.getItem('appsData')) || [];
+//search bar script
+    function myFunction() {
+      const input = document.getElementById("myInput");
+      const filter = input.value.toUpperCase();
+      const dock = document.getElementsByClassName("dock");
 
-    // Vérifier si les données locales sont obsolètes
-    if (JSON.stringify(latestData.apps) !== JSON.stringify(storedData)) {
-      // Mettre à jour les données locales avec la version la plus récente
-      localStorage.setItem('appsData', JSON.stringify(latestData.apps));
-      // Rafraîchir l'affichage si nécessaire
-      filtrerApplications(); // Met à jour l'affichage en cas de changement
+      for (let i = 0; i < dock.length; i++) {
+        const appname = dock[i].getElementsByClassName("appname")[0];
+        const display = appname.innerText.toUpperCase().includes(filter) ? "flex" : "none";
+        dock[i].style.display = display;
+      }
     }
-  } catch (error) {
-    console.log('Error checking for updates:', error);
-  }
-}, 60000); // Vérifier toutes les 60 secondes (ajustez selon vos besoins)
-
-// Fonction pour afficher les applications
-function afficherApplications(appsData) {
-  const appListDiv = document.getElementById("appList");
-  appListDiv.innerHTML = ''; // Effacer le contenu précédent
-
-  appsData.forEach(function(appData) {
-    const dockDiv = document.createElement("div");
-    dockDiv.className = "dock";
-
-    // Créer et ajouter les éléments HTML pour chaque application
-    const appIconImg = document.createElement("img");
-    appIconImg.className = "appicon";
-    appIconImg.src = appData.iconURL || "https://github.com/Ibinou/iTweakHub/blob/main/img/blank.JPG?raw=true";
-    dockDiv.appendChild(appIconImg);
-
-    const appNameDiv = document.createElement("div");
-    appNameDiv.className = "appname";
-    appNameDiv.textContent = appData.name;
-    dockDiv.appendChild(appNameDiv);
-
-    const appDevDiv = document.createElement("div");
-    appDevDiv.className = "appsection";
-    appDevDiv.textContent = appData.developerName;
-    dockDiv.appendChild(appDevDiv);
-
-    const getBtn = document.createElement("button");
-    getBtn.className = "getbtn";
-    getBtn.textContent = "GET";
-    dockDiv.appendChild(getBtn);
-
-    appListDiv.appendChild(dockDiv);
-  });
-}
-
-// Fonction pour filtrer les applications en temps réel
-function filtrerApplications() {
-  const input = document.getElementById("myInput");
-  const filter = input.value.toUpperCase();
-  const storedData = JSON.parse(localStorage.getItem('appsData')) || [];
-
-  const filteredApps = storedData.filter(function(appData) {
-    return appData.name.toUpperCase().includes(filter) || appData.developerName.toUpperCase().includes(filter);
-  });
-
-  afficherApplications(filteredApps);
-}
-
-// Charger les premières applications lors du chargement initial de la page
-chargerApplications(20, 1);
