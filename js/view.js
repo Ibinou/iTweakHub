bb2c4cf1489482559fa1975bb9a6d5712f2474d9
iBaDev <93536549+Ibinou@users.noreply.github.com>
2024-04-27 18:48:12 +0200

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,8 +1,18 @@
+// Fonction pour vérifier si un élément est dans la zone visible de l'écran
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 function afficherDonnees() {
   var appListDiv = document.getElementById("appList");
-  var h1Element = document.getElementById("repoName"); // Élément h1 pour afficher le nom du repo
+  var h1Element = document.getElementById("repoName"); 
   var pElement = document.getElementById("repoDescription");
-  var imgElement = document.getElementById("source_icon");
   var webElement = document.getElementById("source_website");
 
   var urlParams = new URLSearchParams(window.location.search);
@@ -14,38 +24,21 @@ function afficherDonnees() {
         return response.json();
       })
       .then(function(data) {
-        // Afficher les infos
         h1Element.textContent = data.name;
         pElement.textContent = data.description;
-        if (data.iconURL) {
-           imgElement.onload = function() {
-           // L'image a été chargée avec succès
-        };
-           imgElement.onerror = function() {
-           // Le lien d'image n'est pas valide, utiliser un placeholder
-           imgElement.src = "https://raw.githubusercontent.com/Ibinou/iTweakHub/main/img/blank.JPG";
-         };
-           imgElement.src = data.iconURL;
-        } else {
-        // Utiliser un placeholder si aucun lien d'image n'est fourni
-        imgElement.src = "https://raw.githubusercontent.com/Ibinou/iTweakHub/main/img/blank.JPG";
-        }
         if (data.website) {
           webElement.href = data.website;
         } else {
-          // Cacher l'élément website s'il n'y a pas de lien de site web
           webElement.style.display = "none";
         }
 
         var appsData = data.apps;
-
         appsData.sort(function(a, b) {
           return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
         });
 
-        var nombreApplicationsAafficher = appsData.length; // Comptez le nombre d'applications dans le JSON
+        var nombreApplicationsAafficher = appsData.length; 
 
-        // Utilisez une boucle for pour itérer sur le nombre d'applications à afficher
         for (var i = 0; i < nombreApplicationsAafficher; i++) {
           var appData = appsData[i];
 
@@ -57,23 +50,12 @@ function afficherDonnees() {
 
           var appIconImg = document.createElement("img");
           appIconImg.className = "appicon";
-          // Créer un observer d'intersection pour l'image de l'application
-          var observer = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
-              if (entry.isIntersecting) {
-                // Charger l'image lorsque l'élément est visible à l'écran
-                if (appData.iconURL) {
-                  appIconImg.src = appData.iconURL;
-                } else {
-                  // Utiliser un placeholder si aucun lien d'image n'est fourni
-                  appIconImg.src = "https://raw.githubusercontent.com/Ibinou/iTweakHub/main/img/blank.JPG";
-                }
-                observer.unobserve(entry.target); // Arrêter l'observation une fois l'image chargée
-              }
-            });
-          });
-          observer.observe(appIconImg); // Attacher l'observateur à l'élément d'icône d'application
-
+          if (appData.iconURL) {
+            // Utiliser data-src au lieu de src
+            appIconImg.setAttribute("data-src", appData.iconURL);
+          } else {
+            appIconImg.src = "https://raw.githubusercontent.com/Ibinou/iTweakHub/main/img/blank.JPG";
+          }
           appCellLeftDiv.appendChild(appIconImg);
 
           var appCellMetaDiv = document.createElement("div");
@@ -96,7 +78,6 @@ function afficherDonnees() {
           appGetDiv.className = "appget";
 
           var appGetBtn = document.createElement("a");
-          // Ajouter la source directe à l'URL
           appGetBtn.href = 'appinfos.html?name=' + encodeURIComponent(appData.name) + '&source=' + encodeURIComponent(repoUrl);
 
           var getBtn = document.createElement("button");
@@ -109,6 +90,10 @@ function afficherDonnees() {
 
           appListDiv.appendChild(dockDiv);
         }
+
+        // Ajout de l'écouteur d'événements pour le lazy loading
+        window.addEventListener('scroll', lazyLoadImages);
+        lazyLoadImages();
       })
       .catch(function(error) {
         console.log('Error', error);
@@ -116,4 +101,15 @@ function afficherDonnees() {
   } else {
     console.log("Repo URL not provided in the 'repo' parameter.");
   }
+}
+
+// Fonction pour charger les images lorsque l'utilisateur fait défiler jusqu'à elles
+function lazyLoadImages() {
+  var images = document.querySelectorAll('img[data-src]');
+  images.forEach(function(img) {
+    if (isInViewport(img)) {
+      img.src = img.getAttribute('data-src');
+      img.removeAttribute('data-src');
+    }
+  });
 }
