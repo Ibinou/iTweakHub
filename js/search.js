6ea3a1e46703a299615aca711e0d1ef8d525648c
iBaDev <93536549+Ibinou@users.noreply.github.com>
2024-05-20 14:26:47 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,166 +1,148 @@
 // Fonction pour charger les données à partir d'une URL JSON
 function chargerDonneesDepuisURL(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .catch(error => {
-      console.log('Error fetching data from URL', error);
-      return null;
-    });
+    return fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            return { url: url, apps: data.apps }; // Retourner les données avec l'URL source
+        })
+        .catch(function(error) {
+            console.log('Error fetching data from URL', error);
+            return null;
+        });
 }
 
-// Fonction principale pour charger et afficher les données des applications
+// Fonction pour afficher les données
 function afficherDonnees() {
-  var appListDiv = document.getElementById("appList");
+    var appListDiv = document.getElementById("appList");
 
-  // Liste des URLs à charger (y compris apps.json et les URLs de repoURLs du localStorage)
-  var urls = [
-    'https://ibinou.github.io/iTweakHub/apps.json', // URL de apps.json
-  ];
+    // Liste des URLs à charger (y compris apps.json et les URLs de repoURLs du localStorage)
+    var urls = [
+        'https://ibinou.github.io/iTweakHub/apps.json', // URL de apps.json
+    ];
 
-  // Récupérer les URLs de repoURLs du localStorage
-  var repoURLs = JSON.parse(localStorage.getItem('repoURLs')) || [];
+    // Récupérer les URLs de repoURLs du localStorage
+    var repoURLs = JSON.parse(localStorage.getItem('repoURLs')) || [];
 
-  // Ajouter les URLs de repoURLs à la liste d'URLs à charger
-  urls = urls.concat(repoURLs);
+    // Ajouter les URLs de repoURLs à la liste d'URLs à charger
+    urls = urls.concat(repoURLs);
 
-  // Fetcher toutes les données depuis les URLs
-  Promise.all(urls.map(chargerDonneesDepuisURL))
-    .then(function(dataArray) {
-      var allAppsData = [];
-      var iconURLs = [];
-
-      dataArray.forEach(function(data, index) {
-        if (data) {
-          if (data.apps && Array.isArray(data.apps)) {
-            // Filtrer et formater les données essentielles pour chaque application
-            var formattedApps = data.apps.map(function(app) {
-              return {
-                name: app.name,
-                developer: app.developerName,
-                iconURL: app.iconURL,
-                sourceURL: urls[index] || '', // Utiliser l'URL source ou une chaîne vide comme fallback
-              };
+    // Fetcher toutes les données depuis les URLs
+    Promise.all(urls.map(chargerDonneesDepuisURL))
+        .then(function(dataArray) {
+            // Concaténer toutes les applications des différentes sources de données
+            var allAppsData = [];
+            dataArray.forEach(function(data) {
+                if (data && data.apps && Array.isArray(data.apps)) {
+                    // Filtrer et formater les données essentielles pour chaque application avec l'URL source
+                    var formattedApps = data.apps.map(function(app) {
+                        return {
+                            name: app.name,
+                            developer: app.developerName,
+                            iconURL: app.iconURL,
+                            sourceURL: data.url || '', // Utiliser l'URL source ou une chaîne vide comme fallback
+                        };
+                    });
+                    allAppsData = allAppsData.concat(formattedApps);
+                }
             });
-            allAppsData = allAppsData.concat(formattedApps);
-          }
-          
-          // Récupérer les iconURLs des trois dernières sources
-          if (data.iconURL) {
-            iconURLs.push(data.iconURL);
-          }
-        }
-      });
 
-      // Afficher les applications
-      afficherApplications(allAppsData);
+            // Afficher toutes les applications avec les icônes
+            afficherApplications(allAppsData);
 
-      // Afficher les icônes dans les cercles
-      afficherIconesCercles(iconURLs.slice(-3));
+            // Défilement automatique vers le bas pour déclencher le lazy loading
+            window.scrollTo(0, 1); // Défilement d'un pixel vers le bas
+        })
+        .catch(function(error) {
+            console.log('Error fetching or processing data', error);
+        });
 
-      // Afficher les sources dans la modale
-      afficherSourcesModale(dataArray);
-
-      // Défilement automatique vers le bas pour déclencher le lazy loading
-      window.scrollTo(0, 1); // Défilement d'un pixel vers le bas
-    })
-    .catch(function(error) {
-      console.log('Error fetching or processing data', error);
+    // Gestion du lazy loading lors du scroll
+    window.addEventListener('scroll', function() {
+        chargerIconesVisibles();
     });
 
-  // Gestion du lazy loading lors du scroll
-  window.addEventListener('scroll', function() {
-    chargerIconesVisibles();
-  });
+    function chargerIconesVisibles() {
+        var appCells = document.querySelectorAll('.app_cell_left');
 
-  function chargerIconesVisibles() {
-    var appCells = document.querySelectorAll('.app_cell_left');
+        appCells.forEach(function(appCell) {
+            var appIcon = appCell.querySelector('.appicon');
+            if (appIcon && appIcon.dataset.src && isInViewport(appCell)) {
+                // Charger l'icône uniquement si elle n'est pas déjà chargée
+                appIcon.src = appIcon.dataset.src;
+                delete appIcon.dataset.src; // Supprimer l'attribut dataset après chargement
+            }
+        });
+    }
 
-    appCells.forEach(function(appCell) {
-      var appIcon = appCell.querySelector('.appicon');
-      if (appIcon && appIcon.dataset.src && isInViewport(appCell)) {
-        // Charger l'icône uniquement si elle n'est pas déjà chargée
-        appIcon.src = appIcon.dataset.src;
-        delete appIcon.dataset.src; // Supprimer l'attribut dataset après chargement
-      }
-    });
-  }
+    function isInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
-  function isInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+    function afficherApplications(appsData) {
+        appsData.forEach(function(appData) {
+            var dockDiv = document.createElement("div");
+            dockDiv.className = "dock";
 
-  function afficherApplications(appsData) {
-    appsData.forEach(function(appData) {
-      var dockDiv = document.createElement("div");
-      dockDiv.className = "dock";
+            var appCellLeftDiv = document.createElement("div");
+            appCellLeftDiv.className = "app_cell_left";
 
-      var appCellLeftDiv = document.createElement("div");
-      appCellLeftDiv.className = "app_cell_left";
+            var appIconImg = document.createElement("img");
+            appIconImg.className = "appicon";
+            appIconImg.src = "https://github.com/Ibinou/iTweakHub/blob/main/img/blank.JPG?raw=true"; // Placeholder par défaut
 
-      var appIconImg = document.createElement("img");
-      appIconImg.className = "appicon";
-      appIconImg.src = "https://github.com/Ibinou/iTweakHub/blob/main/img/blank.JPG?raw=true"; // Placeholder par défaut
+            if (appData.iconURL) {
+                // Stocker l'URL de l'image dans un attribut dataset pour le lazy loading
+                appIconImg.dataset.src = appData.iconURL;
+            }
 
-      if (appData.iconURL) {
-        // Stocker l'URL de l'image dans un attribut dataset pour le lazy loading
-        appIconImg.dataset.src = appData.iconURL;
-      }
+            appCellLeftDiv.appendChild(appIconImg);
 
-      appCellLeftDiv.appendChild(appIconImg);
+            var appCellMetaDiv = document.createElement("div");
+            appCellMetaDiv.className = "app_cell_meta";
 
-      var appCellMetaDiv = document.createElement("div");
-      appCellMetaDiv.className = "app_cell_meta";
+            var appNameDiv = document.createElement("div");
+            appNameDiv.className = "appname";
+            appNameDiv.textContent = appData.name;
+            appCellMetaDiv.appendChild(appNameDiv);
 
-      var appNameDiv = document.createElement("div");
-      appNameDiv.className = "appname";
-      appNameDiv.textContent = appData.name;
-      appCellMetaDiv.appendChild(appNameDiv);
+            var appDevDiv = document.createElement("div");
+            appDevDiv.className = "appsection";
+            appDevDiv.textContent = appData.developer;
+            appCellMetaDiv.appendChild(appDevDiv);
 
-      var appDevDiv = document.createElement("div");
-      appDevDiv.className = "appsection";
-      appDevDiv.textContent = appData.developer;
-      appCellMetaDiv.appendChild(appDevDiv);
+            appCellLeftDiv.appendChild(appCellMetaDiv);
+            dockDiv.appendChild(appCellLeftDiv);
 
-      appCellLeftDiv.appendChild(appCellMetaDiv);
-      dockDiv.appendChild(appCellLeftDiv);
+            var appGetDiv = document.createElement("div");
+            appGetDiv.className = "appget";
 
-      var appGetDiv = document.createElement("div");
-      appGetDiv.className = "appget";
+            var appGetBtn = document.createElement("a");
+            // Utiliser l'opérateur ternaire pour définir la valeur de source
+            var sourceValue = appData.sourceURL ? encodeURIComponent(appData.sourceURL) : encodeURIComponent('https://ibinou.github.io/iTweakHub/apps.json');
+            appGetBtn.href = 'appinfos.html?name=' + encodeURIComponent(appData.name) + '&source=' + sourceValue;
 
-      var appGetBtn = document.createElement("a");
-      // Utiliser l'opérateur ternaire pour définir la valeur de source
-      var sourceValue = appData.sourceURL ? encodeURIComponent(appData.sourceURL) : encodeURIComponent('https://ibinou.github.io/iTweakHub/apps.json');
-      appGetBtn.href = 'appinfos.html?name=' + encodeURIComponent(appData.name) + '&source=' + sourceValue;
+            var getBtn = document.createElement("button");
+            getBtn.className = "getbtn";
+            getBtn.textContent = "GET";
 
-      var getBtn = document.createElement("button");
-      getBtn.className = "getbtn";
-      getBtn.textContent = "GET";
+            appGetBtn.appendChild(getBtn);
+            appGetDiv.appendChild(appGetBtn);
+            dockDiv.appendChild(appGetDiv);
 
-      appGetBtn.appendChild(getBtn);
-      appGetDiv.appendChild(appGetBtn);
-      dockDiv.appendChild(appGetDiv);
+            appListDiv.appendChild(dockDiv);
+        });
+    }
+}
 
-      appListDiv.appendChild(dockDiv);
-    });
-  }
-
-  function afficherIconesCercles(iconURLs) {
-    const cercles = ['circle1', 'circle2', 'circle3'];
-    iconURLs.forEach((iconURL, index) => {
-      const circleElement = document.getElementById(cercles[index]);
-      if (circleElement) {
-        circleElement.style.backgroundImage = `url(${iconURL})`;
-        circleElement.style.backgroundSize = 'cover';
-      }
-    });
-  }
-
+// Fonction pour afficher les sources dans la modale
 function afficherSourcesModale(dataArray) {
     var modalBody = document.getElementById("modalBody");
     modalBody.innerHTML = ''; // Clear existing content
@@ -169,7 +151,7 @@ function afficherSourcesModale(dataArray) {
     var repoURLs = JSON.parse(localStorage.getItem('repoURLs')) || {};
 
     dataArray.forEach(function(data) {
-        if (data && data.iconURL && data.name && data.identifier && repoURLs[data.identifier]) {
+        if (data && data.iconURL && data.name && repoURLs[data.identifier]) {
             var sourceItemDiv = document.createElement("div");
             sourceItemDiv.className = "source-item";
 
